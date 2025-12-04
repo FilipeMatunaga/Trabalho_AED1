070b76fe6d11a4222a596836fa035873bb5e4494
FilipeMatunaga <filipematunaga@discente.ufg.br>
2025-12-04 00:34:27 -0300

    Update Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -1,1 +1,21 @@
 
+typedef struct{
+    id string;
+    string nome;
+    string professor;
+    string horario;
+    local local;
+    char turma;
+    int duracao_total;
+    int vagas_totais;
+    int vagas_ocupadas;
+    int ano;
+    int periodo;
+}materia;
+
+typedef struct{
+    int sala;
+    string predio;
+    string campus;
+    string cidade;
+}local;
