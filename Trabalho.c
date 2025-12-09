ec809ba724d29862e7b691b25658380d5f2d4a6e
FilipeMatunaga <filipematunaga@discente.ufg.br>
2025-12-08 23:33:51 -0300

    Update Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -1,21 +1,39 @@
+#include <stdio.h>
 
-typedef struct{
-    id string;
-    string nome;
-    string professor;
-    string horario;
-    local local;
-    char turma;
-    int duracao_total;
-    int vagas_totais;
-    int vagas_ocupadas;
-    int ano;
-    int periodo;
-}materia;
-
-typedef struct{
-    int sala;
-    string predio;
-    string campus;
-    string cidade;
-}local;
+int main(){
+    printf("Bem-Vindo ao sistema de matrícula em cursos do INF.\n");
+    while(1){
+        printf("Digite o número para a função desejada:\n");
+        printf("1. Matricular aluno em um curso.\n");
+        printf("2. Desmatricular aluno de um curso.\n");
+        printf("3. Buscar dados do aluno.\n");
+        printf("4. Editar dados do aluno.\n");
+        printf("5. Consultar quantidade de alunos por curso.\n");
+        printf("6. Sair.\n");
+        int n;
+        scanf("%d", &n);
+        switch (n){
+            case 1:
+                /* code */
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                /* code */
+                break;
+            case 4:
+                /* code */
+                break;
+            case 5:
+                /* code */
+                break;
+            case 6:
+                return 0;
+                break;
+            default:
+                printf("Número inválido.\n");
+                break;
+        }
+    }
+}
